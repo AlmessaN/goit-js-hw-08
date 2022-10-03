8398c40fd181655f670de45f0a5b7ef0f368f5ee
Stepan Almes <shtukatulk@gmail.com>
2022-10-03 21:30:02 +0300

    next commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
-console.log(galleryItems);
+const render = galleryItems.map(item => `<div class="gallery__item">
+<a class:"gallery__link" href="${item.original}">
+<img class: "gallery__image" src="${item.preview}"
+data-source="${item.original}"
+alt= "${item.description}"/></a></div>`).join("");
+
+const galleryBox = document.querySelector('.gallery');
+
+galleryBox.insertAdjacentHTML("beforeend", render);
+
+galleryBox.addEventListener("click", e => {
+    e.preventDefault();
+
+    const lightbox = new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionDelay: 250,
+    })
+})
